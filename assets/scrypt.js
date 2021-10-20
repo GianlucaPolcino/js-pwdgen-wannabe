81022a2fa9e460ee0766f0333d3fb78ef82efd96
GianlucaPolcino <gianluca.polcino.gp@gmail.com>
2021-10-20 14:55:31 +0200

    bonus added

--- assets/scrypt.js
+++ assets/scrypt.js
@@ -1,9 +1,12 @@
 const firstName = prompt("Inserisci il tuo nome");
 const lastName = prompt("Inserisci il tuo cognome");
 const favoriteColor = prompt("Inserisci il tuo colore preferito");
+const number = '21';
 
-const password = firstName + lastName + favoriteColor;
+const password = firstName + lastName + favoriteColor + number;
 
 console.log(password);
 
 document.getElementById('pswgenerated').innerHTML = password;
+document.getElementById('pswlenght').innerHTML = password.length;
+
